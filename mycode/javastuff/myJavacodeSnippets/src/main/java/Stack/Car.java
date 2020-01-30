7d9f42ddcfada110281b8c82a1922ae591e88bfd
Kartikey Sawant <kartiksawant1612@gmail.com>
2020-01-30 14:35:05 +0530

    commit

--- mycode/javastuff/myJavacodeSnippets/src/main/java/Stack/Car.java
+++ mycode/javastuff/myJavacodeSnippets/src/main/java/Stack/Car.java
@@ -1,14 +1,18 @@
 package Stack;
 
-public class Car {
+public class Car  {
     private int m_id;
 
    public  Car(int m_id) {
         this.m_id=m_id;
    }
-   int id () {
-       if (m_id==0)
-           return 0;
-       else
-           return m_id;}
+
+
+    public int id() {
+        return m_id;
+    }
+
+
+
 }
+
